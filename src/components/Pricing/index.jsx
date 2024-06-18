32ab67c5bf2ff50d15f4e657dc53af306b4d0965
anarka312 <natalie.talas@gmail.com>
2024-06-18 13:10:35 +0600

    finished section pricing

--- src/components/Pricing/index.jsx
+++ src/components/Pricing/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import styles from "./Pricing.module.css";
 import turn from "./../assets/pricing_img/turn.svg";
-import icon1 from "./../assets/pricing_img/icon1.svg"
-import icon2 from "./../assets/pricing_img/icon2.svg"
-import icon3 from "./../assets/pricing_img/icon3.svg"
+import icon1 from "./../assets/pricing_img/icon1.svg";
+import icon2 from "./../assets/pricing_img/icon2.svg";
+import icon3 from "./../assets/pricing_img/icon3.svg";
 
 const Pricing = () => {
   return (
@@ -12,7 +12,7 @@ const Pricing = () => {
         <div className={styles.pricing_left}>
           <h4>Pricing</h4>
           <h2>Check Our Pricing</h2>
-          <p>Choose the perfect plan for your needs. Always flexible to grow</p>
+          <p>Choose the perfect plan for your needs.<br/> Always flexible to grow</p>
         </div>
         <div className={styles.pricing_right}>
           <span>Monthly</span>
@@ -23,43 +23,58 @@ const Pricing = () => {
 
       <div className={styles.pricing_section2}>
         <div className={styles.pricing_card1}>
+          <div className={styles.pricing_header}>
             <h3>Basic</h3>
             <img src={icon1} alt="icon" />
-            <span>29$/mo</span>
-            <p>Proin viverra, ligula sit amet ultrices semper, ligula arcu tristique sapien</p>
-            <ul>
-                <li>Auto update mode</li>
-                <li>Online operator 24/7</li>
-                <li>International posting</li>
-            </ul>
-            <button>Get started</button>
+          </div>
+          <p className={styles.pricing_price}>
+            29$<span>/mo</span>
+          </p>
+          <p className={styles.pricing_text}>
+            Proin viverra, ligula sit amet ultrices semper, ligula arcu
+            tristique sapien
+          </p>
+          <ul className={styles.pricing_item}>
+            <li>Auto update mode</li>
+            <li>Online operator 24/7</li>
+            <li>International posting</li>
+          </ul>
+          <button>Get started</button>
         </div>
         <div className={styles.pricing_card2}>
+          <div className={styles.pricing_header}>
             <h3>Standard</h3>
             <img src={icon2} alt="icon" />
-            <span>59$/mo</span>
-            <p>Maecenas nec odio et ante tincidunt tempus.</p>
-            <ul>
-                <li>Auto update mode</li>
-                <li>Online operator 24/7</li>
-                <li>International posting</li>
-                <li>Unique newsletters</li>
-            </ul>
-            <button>Get started</button>
+          </div>
+          <p className={styles.pricing_price}>
+            59$<span>/mo</span>
+          </p>
+          <p className={styles.pricing_text}>Maecenas nec odio et ante tincidunt tempus.</p>
+          <ul className={styles.pricing_item}>
+            <li>Auto update mode</li>
+            <li>Online operator 24/7</li>
+            <li>International posting</li>
+            <li>Unique newsletters</li>
+          </ul>
+          <button>Get started</button>
         </div>
         <div className={styles.pricing_card3}>
-            <h3>Prenium</h3>
+          <div className={styles.pricing_header}>
+            <h3>Premium</h3>
             <img src={icon3} alt="icon" />
-            <span>99$/mo</span>
-            <p>Sed lectus. Sed consequat, leo eget bibendum sodales</p>
-            <ul>
-                <li>Auto update mode</li>
-                <li>Online operator 24/7</li>
-                <li>International posting</li>
-                <li>Unique newsletters</li>
-                <li>20 Design templates</li>
-            </ul>
-            <button>Get started</button>
+          </div>
+          <p className={styles.pricing_price}>
+            79$<span>/mo</span>
+          </p>
+          <p className={styles.pricing_text}>Sed lectus. Sed consequat, leo eget bibendum sodales</p>
+          <ul className={styles.pricing_item}>
+            <li>Auto update mode</li>
+            <li>Online operator 24/7</li>
+            <li>International posting</li>
+            <li>Unique newsletters</li>
+            <li>20 Design templates</li>
+          </ul>
+          <button>Get started</button>
         </div>
       </div>
     </div>
