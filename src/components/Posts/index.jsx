f79b5e8f63fdcd8ead8625b96e454b6d1059201a
anarka312 <natalie.talas@gmail.com>
2024-06-28 23:42:42 +0600

    change posts section

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import styles from "./Posts.module.css";
 import left from "./../assets/posts_img/left.svg";
-import right from "./../assets/posts_img/right.svg"
-import avatar from "./../assets/posts_img/avatar.png"
-import avatar2 from "./../assets/posts_img/avatar2.png"
-import avatar3 from "./../assets/posts_img/Avatar3.png"
+import right from "./../assets/posts_img/right.svg";
+import avatar from "./../assets/posts_img/avatar.png";
+import avatar2 from "./../assets/posts_img/avatar2.png";
+import avatar3 from "./../assets/posts_img/Avatar3.png";
 // import fon from "./../assets/posts_img/fon.png"
 
 const Posts = () => {
@@ -15,8 +15,10 @@ const Posts = () => {
         <div className={styles.posts_section}>
           <img src={left} alt="left arrow" className={styles.posts_arrow} />
           <div className={styles.posts_card1}>
-            <h5>15 Dec 2020 8 min read</h5>
-            <h4>Any mechanical keyboard enthusiasts in design?</h4>
+            <div className={styles.posts_desс}>
+              <h5>15 Dec 2020 8 min read</h5>
+              <h4>Any mechanical keyboard enthusiasts in design?</h4>
+            </div>
             <div className={styles.posts_img}>
               <img src={avatar2} alt="img" />
               <div className={styles.posts_img}>
@@ -44,7 +46,7 @@ const Posts = () => {
               </div>
             </div>
           </div>
-        <img src={right} alt="right arrow" className={styles.posts_arrow} />
+          <img src={right} alt="right arrow" className={styles.posts_arrow} />
         </div>
       </div>
     </div>
